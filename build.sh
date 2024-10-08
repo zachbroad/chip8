0623705cba165084790437b1129128e59194e145
Zachary Broad <zachbroad@gmail.com>
2024-10-07 23:33:41 -0400

    Cleanup

--- build.sh
+++ build.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Create build directory if it doesn't exist
 mkdir -p build
-
-# Navigate to build directory
 cd build
-
-# Configure CMake
 cmake ..
-
-# Build the project
 cmake --build .
-
-# Run the executable
 ./chip8
-
-# Return to the original directory
 cd ..
--- getsdl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/libsdl-org/SDL.git -b SDL2
-cd SDL
-mkdir build
-cd build
-../configure
-make
-sudo make install
